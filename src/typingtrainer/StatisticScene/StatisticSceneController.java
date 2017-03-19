b112aec8c9be8e7db0e60b5cc650be02f81e9d84
Mikita <sal_sp.h@mail.ru>
2017-03-19 15:28:03 +0300

    Now all statistic saves in one file that contains all plays. Also you can clear your statistic. Mod setiings also saves in file.

--- src/typingtrainer/StatisticScene/StatisticSceneController.java
+++ src/typingtrainer/StatisticScene/StatisticSceneController.java
@@ -5,9 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import typingtrainer.ManagedScene;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
@@ -27,39 +25,48 @@ public class StatisticSceneController {
     public void initialize() throws IOException
     {
         System.out.println("Сцена статистики готова!");
-        FileReader all_stat = new FileReader("src/typingtrainer/StatisticScene/Statistics/all_stat.txt");
-        BufferedReader reader = new BufferedReader(all_stat);
-        String line;
-        ArrayList<String> lines = new ArrayList<String>();
-        while ((line = reader.readLine())!=null){
-            lines.add(line);
+
+        FileReader st_read = new FileReader("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
+        BufferedReader reader = new BufferedReader(st_read);
+        String ln;
+        ArrayList<String> lns = new ArrayList<String>();
+        while ((ln = reader.readLine())!=null){
+            lns.add(ln);
         }
-        /*for (int i = 0; i < lines.size(); i++)
-            System.out.println(lines.get(i));*/
-        reader.close();
 
-        all_stat.close();
+        if (!lns.isEmpty()) {
+            double mistakes = 0, time = 0, speed = 0;
+            for (int i = 0; i <= lns.size() - 3; i += 3) {
+                mistakes += Double.valueOf(lns.get(i));
+                time += Double.valueOf(lns.get(i + 1));
+                speed += Double.valueOf(lns.get(i + 2));
+            }
+            mistakes /= lns.size() / 3;
+            time /= lns.size() / 3;
+            speed /= lns.size() / 3;
 
-        labelMisAll.setText(String.format("%.2f",Double.valueOf(lines.get(0))));
-        labelTimeAll.setText(String.format("%.2f",Double.valueOf(lines.get(1))) + " сек");
-        labelSpeedAll.setText(String.format("%.2f",Double.valueOf(lines.get(2))) + " зн/мин");
+            //Общая
+            labelMisAll.setText(String.format("%.2f", mistakes));
+            labelTimeAll.setText(String.format("%.2f", time) + " сек");
+            labelSpeedAll.setText(String.format("%.2f", speed) + " зн/мин");
 
-        FileReader last_stat = new FileReader("src/typingtrainer/StatisticScene/Statistics/last_stat.txt");
-        BufferedReader reader_last = new BufferedReader(last_stat);
-        String line_last;
-        ArrayList<String> lines_last = new ArrayList<String>();
-        while ((line_last = reader_last.readLine())!=null){
-            lines_last.add(line_last);
+            //Последний сеанс
+            labelMisLast.setText(lns.get(lns.size() - 3));
+            labelTimeLast.setText(String.format("%.2f", Double.valueOf(lns.get(lns.size() - 2))) + " сек");
+            labelSpeedLast.setText(lns.get(lns.size() - 1) + " зн/мин");
+
+            reader.close();
+        }else {
+            //Общая
+            labelMisAll.setText(String.valueOf(0));
+            labelTimeAll.setText(String.valueOf(0));
+            labelSpeedAll.setText(String.valueOf(0));
+
+            //Последний сеанс
+            labelMisLast.setText(String.valueOf(0));
+            labelTimeLast.setText(String.valueOf(0));
+            labelSpeedLast.setText(String.valueOf(0));
         }
-        /*for (int i = 0; i < lines_last.size(); i++)
-            System.out.println(lines_last.get(i));*/
-        reader.close();
-
-        last_stat.close();
-
-        labelMisLast.setText(lines_last.get(0));
-        labelTimeLast.setText(lines_last.get(1) + " сек");
-        labelSpeedLast.setText(lines_last.get(2) + " зн/мин");
     }
 
     public void onBackClicked(MouseEvent mouseEvent)
@@ -72,5 +79,23 @@ public class StatisticSceneController {
         {
             System.out.println(e.getMessage());
         }
+    }
+
+    public void onClearClicked(MouseEvent mouseEvent) throws IOException {
+        FileWriter st_write = new FileWriter("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
+        st_write.write("");
+        st_write.flush();
+        st_write.close();
+
+
+        //Общая
+        labelMisAll.setText(String.valueOf(0));
+        labelTimeAll.setText(String.valueOf(0));
+        labelSpeedAll.setText(String.valueOf(0));
+
+        //Последний сеанс
+        labelMisLast.setText(String.valueOf(0));
+        labelTimeLast.setText(String.valueOf(0));
+        labelSpeedLast.setText(String.valueOf(0));
     }
 }
