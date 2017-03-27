a8da2c2a9acc140ab482f56b07fdf24b6fd36b7b
Ruslan Komarichev <komarichev_pi15b@mail.ru>
2017-03-27 20:46:03 +0300

    График закрывается Сцена с графиком закрывается по нажатию на него Заменён треклист CSS переформированы, теперь каждая сцена связана с main.css, где прописаны общие правила

--- src/typingtrainer/StatisticScene/StatisticSceneController.java
+++ src/typingtrainer/StatisticScene/StatisticSceneController.java
@@ -3,6 +3,7 @@ package typingtrainer.StatisticScene;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,20 +27,21 @@ import java.util.List;
 /**
  * Created by Никитка on 12.03.2017.
  */
-public class StatisticSceneController {
+public class StatisticSceneController
+{
 
 	@FXML
-    public Label labelMisAll;
+	public Label labelMisAll;
 	@FXML
-    public Label labelMisLast;
+	public Label labelMisLast;
 	@FXML
-    public Label labelTimeAll;
+	public Label labelTimeAll;
 	@FXML
-    public Label labelTimeLast;
+	public Label labelTimeLast;
 	@FXML
-    public Label labelSpeedAll;
+	public Label labelSpeedAll;
 	@FXML
-    public Label labelSpeedLast;
+	public Label labelSpeedLast;
 	@FXML
 	public Label labelAvgHeader;
 
@@ -47,147 +49,159 @@ public class StatisticSceneController {
 	private ArrayList<Integer> mistakes_list = new ArrayList<Integer>();
 	private ArrayList<Double> time_list = new ArrayList<Double>();
 
-    public void initialize() throws IOException
-    {
-        System.out.println("Сцена статистики готова!");
+	public void initialize()
+	{
+		System.out.println("Сцена статистики готова!");
 
 
+		try
+		{
+			FileReader st_read = new FileReader("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
+			BufferedReader reader = new BufferedReader(st_read);
+			int lastMistakes = 0, lastSpeed = 0, count = 0;
+			double lastTime = 0.0, avgTime = 0.0, avgMistakes = 0.0, avgSpeed = 0.0;
+			String buf;
+			//List<int> speed_list = new List<int>();
 
+			while ((buf = reader.readLine()) != null)
+			{
+				lastMistakes = Integer.parseInt(buf);
+				mistakes_list.add(lastMistakes);
+				buf = reader.readLine();
+				lastTime = Double.parseDouble(buf);
+				time_list.add(lastTime);
+				buf = reader.readLine();
+				lastSpeed = Integer.parseInt(buf);
+				speed_list.add(lastSpeed);
 
-        try {
-            FileReader st_read = new FileReader("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
-            BufferedReader reader = new BufferedReader(st_read);
-            int lastMistakes = 0, lastSpeed = 0, count = 0;
-            double lastTime = 0.0, avgTime = 0.0, avgMistakes = 0.0, avgSpeed = 0.0;
-            String buf;
-            //List<int> speed_list = new List<int>();
+				avgMistakes += lastMistakes;
+				avgTime += lastTime;
+				avgSpeed += lastSpeed;
 
+				++count;
+			}
+			reader.close();
+			st_read.close();
 
-            while ((buf = reader.readLine()) != null) {
-                lastMistakes = Integer.parseInt(buf);
-                buf = reader.readLine();
-                mistakes_list.add(lastMistakes);
-                lastTime = Double.parseDouble(buf);
-                buf = reader.readLine();
-                time_list.add(lastTime);
-                lastSpeed = Integer.parseInt(buf);
-                speed_list.add(lastSpeed);
+			//Общая
+			if (count > 0)
+			{
+				avgMistakes /= 1.0 * count;
+				avgTime /= 1.0 * count;
+				avgSpeed /= 1.0 * count;
+			}
+			labelAvgHeader.setText("Среднее за " + count + (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 12 || count % 100 > 14) ? " раза" : " раз"));
+			labelMisAll.setText(String.format("%.2f", avgMistakes));
+			labelTimeAll.setText(String.format("%.2f", avgTime) + " сек");
+			labelSpeedAll.setText(String.format("%.2f", avgSpeed) + " зн/мин");
 
-                avgMistakes += lastMistakes;
-                avgTime += lastTime;
-                avgSpeed += lastSpeed;
+			//Последний сеанс
+			labelMisLast.setText("" + lastMistakes);
+			labelTimeLast.setText(String.format("%.2f", lastTime) + " сек");
+			labelSpeedLast.setText(lastSpeed + " зн/мин");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Файл статистики отсутствует");
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setTitle("Ошибка");
+			alert.setHeaderText(null);
+			alert.setContentText("Файл статистики отсутствует");
+			alert.showAndWait();
 
-                ++count;
-            }
-            reader.close();
-            st_read.close();
+			//Общая
+			labelAvgHeader.setText("Среднее за 0 раз");
+			labelMisAll.setText("0,00");
+			labelTimeAll.setText("0,00 сек");
+			labelSpeedAll.setText("0,00 зн/мни");
 
-            //Общая
-            if (count > 0) {
-                avgMistakes /= 1.0 * count;
-                avgTime /= 1.0 * count;
-                avgSpeed /= 1.0 * count;
-            }
-            labelAvgHeader.setText("Среднее за " + count + (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 12 || count % 100 > 14) ? " раза" : " раз"));
-            labelMisAll.setText(String.format("%.2f", avgMistakes));
-            labelTimeAll.setText(String.format("%.2f", avgTime) + " сек");
-            labelSpeedAll.setText(String.format("%.2f", avgSpeed) + " зн/мин");
+			//Последний сеанс
+			labelMisLast.setText("0");
+			labelTimeLast.setText("0,00 сек");
+			labelSpeedLast.setText("0 зн/мин");
+		}
+	}
 
-            //Последний сеанс
-            labelMisLast.setText("" + lastMistakes);
-            labelTimeLast.setText(String.format("%.2f", lastTime) + " сек");
-            labelSpeedLast.setText(lastSpeed + " зн/мин");
-        }catch (Exception e){
-            System.out.println("Файл статистики отсутствует");
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Ошибка");
-            alert.setHeaderText(null);
-            alert.setContentText("Файл статистики отсутствует");
-            alert.showAndWait();
+	public void onBackClicked(MouseEvent mouseEvent)
+	{
+		try
+		{
+			((ManagedScene) (((Label) mouseEvent.getSource()).getScene())).getManager().popScene();
+		}
+		catch (InvocationTargetException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
 
-            //Общая
-            labelAvgHeader.setText("Среднее за 0 раз");
-            labelMisAll.setText("0,00");
-            labelTimeAll.setText("0,00 сек");
-            labelSpeedAll.setText("0,00 зн/мни");
+	public void onClearClicked(MouseEvent mouseEvent) throws IOException
+	{
+		FileWriter st_write = new FileWriter("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
+		st_write.write("");
+		st_write.flush();
+		st_write.close();
 
-            //Последний сеанс
-            labelMisLast.setText("0");
-            labelTimeLast.setText("0,00 сек");
-            labelSpeedLast.setText("0 зн/мин");
-        }
-    }
-
-    public void onBackClicked(MouseEvent mouseEvent)
-    {
-        try
-        {
-            ((ManagedScene)(((Label)mouseEvent.getSource()).getScene())).getManager().popScene();
-        }
-        catch (InvocationTargetException e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void onClearClicked(MouseEvent mouseEvent) throws IOException {
-        FileWriter st_write = new FileWriter("src/typingtrainer/StatisticScene/Statistics/statistic.txt");
-        st_write.write("");
-        st_write.flush();
-        st_write.close();
-
-        //Общая
+		//Общая
 		labelAvgHeader.setText("Среднее за 0 раз");
-        labelMisAll.setText("0,00");
-        labelTimeAll.setText("0,00 сек");
-        labelSpeedAll.setText("0,00 зн/мни");
+		labelMisAll.setText("0,00");
+		labelTimeAll.setText("0,00 сек");
+		labelSpeedAll.setText("0,00 зн/мни");
 
-        //Последний сеанс
-        labelMisLast.setText("0");
-        labelTimeLast.setText("0,00 сек");
-        labelSpeedLast.setText("0 зн/мин");
-    }
+		//Последний сеанс
+		labelMisLast.setText("0");
+		labelTimeLast.setText("0,00 сек");
+		labelSpeedLast.setText("0 зн/мин");
+	}
 
-    public void onGraphClicked(MouseEvent mouseEvent) throws IOException {
+	public void onGraphClicked(MouseEvent mouseEvent)
+	{
+		NumberAxis x = new NumberAxis();
+		NumberAxis y = new NumberAxis();
 
-        NumberAxis x = new NumberAxis();
-        NumberAxis y = new NumberAxis();
+		LineChart<Number, Number> numberLineChart = new LineChart<Number, Number>(x, y);
+		numberLineChart.setTitle("Статистика");
+		XYChart.Series series1 = new XYChart.Series();
+		XYChart.Series series2 = new XYChart.Series();
+		XYChart.Series series3 = new XYChart.Series();
 
-        LineChart<Number, Number> numberLineChart = new LineChart<Number, Number>(x,y);
-        numberLineChart.setTitle("Статистика");
-        XYChart.Series series1 = new XYChart.Series();
-        XYChart.Series series2 = new XYChart.Series();
-        XYChart.Series series3 = new XYChart.Series();
+		series1.setName("Скорость (зн/мин)");
+		series2.setName("Ошибки (шт)");
+		series3.setName("Время (сек)");
 
-        series1.setName("Скорость(зн/мин)");
-        series2.setName("Ошибки(шт)");
-        series3.setName("Время(сек)");
+		ObservableList<XYChart.Data> datas = FXCollections.observableArrayList();
+		ObservableList<XYChart.Data> datas2 = FXCollections.observableArrayList();
+		ObservableList<XYChart.Data> datas3 = FXCollections.observableArrayList();
 
-        ObservableList<XYChart.Data> datas = FXCollections.observableArrayList();
-        ObservableList<XYChart.Data> datas2 = FXCollections.observableArrayList();
-        ObservableList<XYChart.Data> datas3 = FXCollections.observableArrayList();
+		for (int i = 0; i < this.speed_list.size(); i++)
+		{
+			datas.add(new XYChart.Data(i, this.speed_list.get(i)));
+			datas2.add(new XYChart.Data(i, this.mistakes_list.get(i)));
+			datas3.add(new XYChart.Data(i, this.time_list.get(i)));
+		}
 
-        for (int i = 0; i < this.speed_list.size();i++){
-            datas.add(new XYChart.Data(i,this.speed_list.get(i)));
-            datas2.add(new XYChart.Data(i,this.mistakes_list.get(i)));
-            datas3.add(new XYChart.Data(i,this.time_list.get(i)));
-        }
-
-        series1.setData(datas);
-        series2.setData(datas2);
-        series3.setData(datas3);
+		series1.setData(datas);
+		series2.setData(datas2);
+		series3.setData(datas3);
 
 
+		final SceneManager sceneManager = ((ManagedScene) (((Label) mouseEvent.getSource()).getScene())).getManager();
+		numberLineChart.setOnMouseClicked(event ->
+		{
+			try
+			{
+				sceneManager.popScene();
+			}
+			catch (InvocationTargetException e)
+			{
+				e.printStackTrace();
+			}
+		});
+		ManagedScene graphScene = new ManagedScene(numberLineChart, 600, 600, sceneManager);
 
+		numberLineChart.getData().add(series1);
+		numberLineChart.getData().add(series2);
+		numberLineChart.getData().add(series3);
+		sceneManager.pushScene(graphScene);
 
-        SceneManager sceneManager = ((ManagedScene)(((Label)mouseEvent.getSource()).getScene())).getManager();
-       // Parent practiceSceneFXML = FXMLLoader.load(Main.class.getResource("ModScene/modScene.fxml"));
-        ManagedScene practiceScene = new ManagedScene(numberLineChart, 600, 600, sceneManager);
-
-        numberLineChart.getData().add(series1);
-        numberLineChart.getData().add(series2);
-        numberLineChart.getData().add(series3);
-        sceneManager.pushScene(practiceScene);
-
-    }
+	}
 }
