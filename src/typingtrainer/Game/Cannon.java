549985b01cf3c5dfcd52882d1196f77c2165cfa5
Ruslan Komarichev <komarichev_pi15b@mail.ru>
2017-05-03 16:17:02 +0400

    Пушки отъезжают назад при выстреле, но только по оси Ох

--- src/typingtrainer/Game/Cannon.java
+++ src/typingtrainer/Game/Cannon.java
@@ -1,5 +1,6 @@
 package typingtrainer.Game;
 
+import com.sun.javafx.geom.Vec2d;
 import com.sun.javafx.geom.Vec2f;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
@@ -14,9 +15,12 @@ import typingtrainer.Main;
 public abstract class Cannon extends PvpObject
 {
 	private Ship parentShip;
+	private Point2D basePosition;
+	private Thread pushingThread;
 	public Cannon(Ship parentShip, Belonging belonging, Point2D position)
 	{
 		super(belonging, position);
+		basePosition = new Point2D(position.getX(), position.getY());
 		this.parentShip = parentShip;
 		image = new WritableImage(Game.SPRITE_SHEET.getPixelReader(), 132, 0, 150, 55);
 	}
@@ -35,6 +39,41 @@ public abstract class Cannon extends PvpObject
 		Cannonball cannonball = new Cannonball(this, belonging, new Point2D(position.getX() + pivot.getX() - 8, position.getY() + pivot.getY()  - 8));
 		cannonball.setTarget(target);
 		getParentShip().getParentGame().getCannonballs().add(cannonball);
+
+		if (pushingThread != null && pushingThread.isAlive())
+			pushingThread.interrupt();
+		pushingThread = new Thread(() ->
+		{
+			try
+			{
+				for (int i = 0; i < 3; ++i)
+				{
+					//Point2D backPos = new Point2D(position.getX())
+					position = position.subtract(20, 0);
+					Thread.sleep(50);
+
+				}
+				double step = position.distance(basePosition) / 12;
+				Point2D dir = basePosition.subtract(position).normalize();
+				for (int i = 0; i < 12; ++i)
+				{
+					//Point2D backPos = new Point2D(position.getX())
+					position = position.add(dir.getX() * step, dir.getY() * step);
+					Thread.sleep(50);
+					
+				}
+			}
+			catch (InterruptedException e)
+			{
+				System.out.println(e.getMessage());
+			}
+		});
+		pushingThread.start();
+	}
+
+	public Point2D getBasePosition()
+	{
+		return basePosition;
 	}
 
 	public Ship getParentShip()
